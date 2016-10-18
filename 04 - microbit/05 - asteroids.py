011c0d0e645edacfa359e0822065014799fd01fe
Derek Graham <deejaygraham@gmail.com>
2016-10-18 12:56:03 +0100

    explosions and polishing
    can’t get buttons to work yet.

--- 04 - microbit/05 - asteroids.py
+++ 04 - microbit/05 - asteroids.py
@@ -3,7 +3,7 @@ from microbit import *
 import random
 
 # board dimensions
-pixel_width, pixel_height = 5, 5
+pixel_width, pixel_height = 4, 4
 Frame_Rate_In_Milliseconds = 500
 
 # general game functions
@@ -13,10 +13,10 @@ def display_count_down(fromValue):
         display.show(str(i))
         sleep(1000)
 
-def display_game_over(score):
+def display_game_over(ship, score):
     # blow up ship
     # take x of ship and blow up outwards
-
+    blow_up_ship(ship)
     display.show(Image.SAD)
     sleep(500)
     display.scroll("Score: " + str(score))
@@ -33,22 +33,37 @@ def hide_ship(ship):
 def draw_ship(ship):
     display.set_pixel(ship[0], ship[1], ship[2])
 
+def move_ship_left(ship):
+    
+    ship[0] = max(ship[0], 0)
+    return ship
+        
+def move_ship_right(ship):
+    
+    ship[0] = min(ship[0], pixel_width)
+    return ship
+        
 def blow_up_ship(ship):
 
     hide_ship(ship)
 
+    draw_explosion(ship, ship[2], 1)
+    sleep(200)
+    display.clear()
+        
     for brightness in range(ship[2], 0, -1):
-        draw_explosion(ship, brightness)
-        sleep(500)
+        draw_explosion(ship, brightness, 2)
+        sleep(200)
+        display.clear()
 
     # look at x position
-def draw_explosion(centre, brightness):
+def draw_explosion(centre, brightness, radius):
 
     x = centre[0]
     y = centre[1]
-    left_of_centre = x - 1
-    right_of_centre = x + 1
-    above_centre = y - 1
+    left_of_centre = x - radius
+    right_of_centre = x + radius
+    above_centre = y - radius
 
     if left_of_centre >= 0:
         display.set_pixel(left_of_centre, y, brightness)
@@ -56,7 +71,7 @@ def draw_explosion(centre, brightness):
 
     display.set_pixel(x, above_centre, brightness)
 
-    if right_of_centre < pixel_width:
+    if right_of_centre <= pixel_width:
         display.set_pixel(right_of_centre, y, brightness)
         display.set_pixel(right_of_centre, above_centre, brightness)
 
@@ -70,10 +85,12 @@ def draw_asteroid(asteroid):
 
 def create_asteroid():
     # x, y, brightness, maybe speed.., size
-    return [ random.randint(0, pixel_width), 0, random.randint(1, 8) ]
+    minimum_brightness = 3
+    maximum_brightness = 8
+    return [ random.randint(0, pixel_width), 0, random.randint(minimum_brightness, maximum_brightness) ]
 
 def asteroid_collides_with_ship(asteroid, ship):
-    return asteroid[0] == ship[0] and asteroid[1] == ship[1]:
+    return (asteroid[0] == ship[0]) and (asteroid[1] == ship[1])
 
 def move_asteroid(asteroid):
     asteroid[1] += 1
@@ -95,21 +112,28 @@ while True:
     draw_ship(ship)
     draw_asteroid(asteroid)
 
+    # increase speed once we are above a certain score...
     sleep(Frame_Rate_In_Milliseconds)
 
     # check for button pushes...
+    if button_a.is_pressed():
+        ship = move_ship_left(ship)
 
+    if button_b.is_pressed():
+        ship = move_ship_right(ship)
+        
     if asteroid_collides_with_ship(asteroid, ship):
-        display_game_over(score)
+        display_game_over(ship, score)
         break
 
     hide_ship(ship)
     hide_asteroid(asteroid)
 
-    if asteroid[1] == 4:
+    if asteroid[1] == pixel_height:
         score += 1
+        
         asteroid = create_asteroid()
+    else:
+        asteroid = move_asteroid(asteroid)
 
-    asteroid = move_asteroid(asteroid)
-
-    # increase speed once we are above a certain score...
+    
