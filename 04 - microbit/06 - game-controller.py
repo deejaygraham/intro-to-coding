eea5f171e8d231e8b5662cb30222eef22b064c80
Derek Graham <deejaygraham@gmail.com>
2017-05-10 12:50:31 +0100

    watchdog tidy and added note on tty for MacBook

--- 04 - microbit/06 - game-controller.py
+++ 04 - microbit/06 - game-controller.py
@@ -6,10 +6,6 @@ forty_five_degrees = 500
 x_tilt_limit = forty_five_degrees
 y_tilt_limit = forty_five_degrees
 
-# blink one pixel to show the controller is alive
-watch_dog_pixel = Image("00000:00000:00600:00000:00000")
-watch_dog_flash = [watch_dog_pixel, Image()]
-
 def read_orientation():
 
   orientation = ""
@@ -44,6 +40,12 @@ def read_buttons():
 
 def write_to_serial_port(data):
     print(data)
+
+# display flashing middle pixel so we know 
+# the device is alive
+# alternate between blank display and single pixel
+watch_dog_pixel = Image("00000:00000:00600:00000:00000")
+watch_dog_flash = [watch_dog_pixel, Image()]
 
 display.show(watch_dog_flash, loop=True, wait=False, delay=1000)
 
