73678a2904e09e7ed7f4306bd9244af6d2dfecfa
Derek Graham <deejaygraham@gmail.com>
2017-06-29 17:00:43 +0100

    tidied and simplified

--- 04 - microbit/06 - treasure-hunt-clues.py
+++ 04 - microbit/06 - treasure-hunt-clues.py
@@ -2,52 +2,50 @@
 from microbit import *
 import radio
 
-# Starting image
+# Pirates !!!
 skull = Image("09990:90909:99099:99999:04440")
 cross_bones = Image("94009:49090:00900:09094:90049")
 skull_and_cross_bones = [skull, cross_bones]
-
 display.show(skull_and_cross_bones)
 
 all_clues = [
-    "Fifteen paces east",
+    "Walk Fifteen paces east",
     "Ten paces north",
     "Walk forwards",
     "Look under the seat",
-    "hooray, you win!"
+    "Hooray, you win!"
 ]
 
 clue_count = len(all_clues)
 
 # the clue this unit will broadcast
 # can be changed by the a and b buttons
-clue = 0
+clue = 1
 
 radio.on()
 
+beacon_off = Image("00000:00000:00900:09090:99999")
+beacon_on  = Image("00000:00900:00900:09090:99999")
+beacon_animation = [beacon_off, beacon_on, beacon_off]
+
 while True:
+    
     # show which clue we are broadcasting
-    display.scroll(str(clue))
+    display.show(beacon_animation)
 
     # decrease clue number
     if button_a.was_pressed():
-        clue -= 1
-        if clue < 0:
-            clue = clue_count - 1
+        clue = max(clue - 1, 1)
+        display.show(str(clue))
     # increase clue number
     elif button_b.was_pressed():
-        clue += 1
-        if clue >= clue_count:
-            clue = 0
-    # check for incoming message
+        clue = min(clue +  1, clue_count)
+        display.show(str(clue))
+    # broadcast the current clue
     else:
-        message = radio.receive()
-
-        if message:
-            # expecting a clue number
-            requested_clue = int(message)
-
-            if clue == requested_clue:
-                radio.send(all_clues[clue])
-
-        sleep(500)
+        clue_text = str(clue) + ":" + all_clues[clue - 1]
+        radio.send(clue_text)
+                
+    sleep(1000)
+        
+        
